9af2ccc0d0f0507c8a96a7a3e3cd5bc56a60036d
YagizEbil <kyagizebil@gmail.com>
2022-11-24 21:45:05 +0300

    Yağız

--- Search in PDF/In Text Search Script.py
+++ Search in PDF/In Text Search Script.py
@@ -1,11 +1,16 @@
+from PyPDF2 import PdfFileReader
+from pathlib import Path
 import re
 
+pdfpath = str(input("Enter PDF path: "))
+pdf = PdfFileReader(pdfpath)
+pdfsearch = input("What you want to search: ")
 pages_sentences = []
 page_num = 1
 for page in pdf.pages:
     page_text = page.extractText()
-    if 'Waldo' in page_text:
-        sentence_list = ["Page "+ str(page_num) + ': ' + sentence.replace('\n', '') for sentence in re.split('\.\W+|\?\W+|\!\W+', page_text) if 'Waldo' in sentence][0]
+    if pdfsearch in page_text:
+        sentence_list = ["Page "+ str(page_num) + ': ' + sentence.replace('\n', '') for sentence in re.split('\.\W+|\?\W+|\!\W+', page_text) if pdfsearch in sentence][0]
         pages_sentences.append(sentence_list)
     page_num = int(page_num)
     page_num = page_num + 1
