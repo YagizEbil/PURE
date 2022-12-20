d5b37aac87eabd2f3af965e8abdf521c70e63c7a
YagizEbil <44137337+YagizEbil@users.noreply.github.com>
2022-12-20 23:05:39 +0300

    Update PDFtoTXT.py

--- Search in PDF/PDFtoTXT.py
+++ Search in PDF/PDFtoTXT.py
@@ -8,7 +8,7 @@ pdf = PdfFileReader(pdfpath)
 page_1_object = pdf.getPage(0) 
 page_1_text = page_1_object.extractText()
 
-with Path('denemetext.txt').open(mode='w') as output_file: 
+with Path('file.txt').open(mode='w') as output_file: 
     text = ''
     for page in pdf.pages:
         text += page.extractText()
